4b8e00a8c583906a0dc1789a0641fdc89e4adee9
ffrmns <fikrifa0@gmail.com>
2022-02-23 21:08:45 +0700

    fill server.js and routes.js with empty handling function

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: () => {},
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: () => {},
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: () => {},
+  },
+];
+
+module.exports = routes;
